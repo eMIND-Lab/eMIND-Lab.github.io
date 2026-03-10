10d407122c0f95b83400efe11c7055c096eb861a
eMIND-Lab <emind1205@gmail.com>
2026-03-10 12:53:10 +0000

    Deploying to gh-pages from @ eMIND-Lab/eMIND-Lab.github.io@b03bc3e62970179fe1b9dace432c6bde8287c890 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "eMIND-Lab/eMIND-lab.github.io",
+    "data-repo": "eMIND-Lab/eMIND-Lab.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
